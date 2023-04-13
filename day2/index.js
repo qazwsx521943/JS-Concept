4896e2ea5177da0587b0acdd15301b3f39ef3a3d
Chung Hsuan <qazwsx5219430630@gmail.com>
2023-04-13 14:21:06 +0800

    Day 2 - css + js clock

---
+++ day2/index.js
@@ -0,0 +1,20 @@
+const second_pointer = document.querySelector(".seconds");
+const minute_pointer = document.querySelector(".minutes");
+const hour_pointer = document.querySelector(".hours");
+
+function setDate() {
+    const now = new Date();
+    const second = now.getSeconds();
+    let hour = now.getHours();
+    hour = hour > 12 ? hour - 12 : hour;
+    const minute = now.getMinutes();
+    console.log("now", now);
+    console.log("seconds", second);
+    console.log("minutes", minute);
+    console.log("hours", hour);
+    second_pointer.style.transform = `rotate(${90 + 6 * second}deg)`;
+    minute_pointer.style.transform = `rotate(${90 + 6 * minute}deg)`;
+    hour_pointer.style.transform = `rotate(${90 + 30 * hour}deg)`;
+}
+
+setInterval(setDate, 1000);
