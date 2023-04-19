ccb74b048dd6dbf74776ca21bcb3e1f02d722fe2
Chung Hsuan <qazwsx5219430630@gmail.com>
2023-04-19 12:24:25 +0800

    palindrome pointer practice

--- algorithm/averagePair.js
+++ algorithm/averagePair.js
@@ -1,16 +1,37 @@
+// O(n^2)
+// function averagePair(arr, num) {
+//     let pair = [];
+//     let count = 0;
+//     for (let [keyi, valuei] of arr.entries()) {
+//         for (let j = keyi + 1; j < arr.length; j++) {
+//             console.log(valuei, arr[j]);
+//             count++;
+//             if (valuei + arr[j] === 2 * num) {
+//                 pair.push([valuei, arr[j]]);
+//             }
+//         }
+//     }
+//     console.log("steps:", count);
+//     console.log(pair);
+// }
+
+// O(n)
 function averagePair(arr, num) {
+    let min = 0;
+    let max = arr.length - 1;
     let pair = [];
-    let count = 0;
-    for (let [keyi, valuei] of arr.entries()) {
-        for (let j = keyi + 1; j < arr.length; j++) {
-            console.log(valuei, arr[j]);
-            count++;
-            if (valuei + arr[j] === 2 * num) {
-                pair.push([valuei, arr[j]]);
-            }
+    while (min <= max) {
+        if (arr[min] + arr[max] === 2 * num) {
+            pair.push([arr[min], arr[max]]);
+            min++;
+            max--;
+        } else if (arr[min] + arr[max] > 2 * num) {
+            max--;
+        } else if (arr[min] + arr[max] < 2 * num) {
+            min++;
         }
     }
-    console.log("steps:", count);
+
     console.log(pair);
 }
 const arr = [1, 2, 3, 4, 5, 6, 7, 8];
---
+++ algorithm/palindrome.js
@@ -0,0 +1,22 @@
+function palindromeCheck(str) {
+    let strToArr = str.split("");
+    let min = 0;
+    let max = strToArr.length - 1;
+    while (min <= max) {
+        if (strToArr[min] === strToArr[max]) {
+            console.log(strToArr[min], strToArr[max]);
+            min++;
+            max--;
+        }
+        if (strToArr[min] !== strToArr[max]) {
+            console.log("not a Palindrome!!");
+            return false;
+        }
+    }
+    console.log("Palindrome!!");
+    return true;
+}
+
+palindromeCheck("awesome");
+palindromeCheck("foobar");
+palindromeCheck("tacocat");
