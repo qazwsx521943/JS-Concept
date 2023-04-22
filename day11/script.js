43ec39b1300c431396bfd66b638f558401da1bca
Chung Hsuan <qazwsx5219430630@gmail.com>
2023-04-22 14:36:56 +0800

    day 12 key sequence

--- day11/script.js
+++ day11/script.js
@@ -18,6 +18,40 @@ function updateBtn(e) {
     const icon = this.paused ? "►" : "▷";
     playBtn.textContent = icon;
 }
+
+function skip(e) {
+    video.currentTime += parseInt(this.dataset.skip);
+    // console.log(video.currentTime);
+    // console.log(this.dataset);
+}
+
+function progressShift(e) {
+    console.log(video.duration);
+    // video.currentTime = progressBar.style.flexBasis * video.duration;
+    progressBar.style.flexBasis = `${(this.currentTime / this.offsetWidth) * 100}%`;
+    console.log(progressBar.style);
+    // video.currentTime =
+}
+
+function range(e) {
+    video[this.name] = this.value;
+    // console.log(this.value);
+    // console.log(this.name);
+}
+function skipTo(e) {
+    console.log(e);
+    const percentage = `${(e.offsetX / 640) * 100}%`;
+    progressBar.style.flexBasis = percentage;
+    video.currentTime = (e.offsetX / 640) * video.duration;
+}
+let mousedown = false;
+progress.addEventListener("click", skipTo);
+progress.addEventListener("mousemove", (e) => mousedown && skipTo(e));
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mousedown", () => (mousedown = false));
+video.addEventListener("timeupdate", progressShift);
+ranges.forEach((r) => r.addEventListener("change", range));
+skipBtns.forEach((sb) => sb.addEventListener("click", skip));
 video.addEventListener("play", updateBtn);
 video.addEventListener("pause", updateBtn);
 video.addEventListener("click", playVideo);
